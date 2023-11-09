888b9a2afa6b28f2caeff7040b52c560c339beb1
iza <kan077bct039@kec.edu.np>
2023-11-09 10:38:24 +0545

    add donation page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('./db');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,8 +9,9 @@ var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signuppage');
-var profileRouter = require('./routes/profilepage')
+var profileRouter = require('./routes/profilepage');
 var signinRouter = require('./routes/signinpage');
+var donationRouter = require('./routes/donations');
 
 
 var app = express();
@@ -22,6 +24,7 @@ app.use(bodyParser.json())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,6 +36,7 @@ app.use('/users', usersRouter);
 app.use('/signuppage', signupRouter);
 app.use('/profilepage', profileRouter);
 app.use('/signinpage',signinRouter);
+app.use('/donation',donationRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
